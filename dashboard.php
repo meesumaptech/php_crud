9f3aef71a22f1eb3acb313bbeacfcdb9278b59dd
meesumaptech <meesumaptech@gmail.com>
2026-02-20 16:57:55 +0500

    crud operations

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if(isset($_SESSION['username'])=="")
+{
+    header("Location:login.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,5 +15,12 @@ session_start();
 </head>
 <body>
     <h1>Hello <?php echo $_SESSION['username']; ?></h1>
+
+    <!-- <form action="" method="post">
+        <input type="submit" name="btnlogout" value="Logout">
+    </form> -->
+
+    <a href="logout.php">Logout</a>
 </body>
 </html>
+
--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
     include("dbconnection.php");
     session_start();
+
+    if(isset($_SESSION['username']))
+{
+    header("Location:dashboard.php");
+}
 ?>
 
 <!DOCTYPE html>
