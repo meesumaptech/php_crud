48580475908a9ac1223a5767bfd67e5dc4317f93
meesumaptech <meesumaptech@gmail.com>
2026-03-02 16:50:49 +0500

    file uploading

--- image.php
+++ image.php
@@ -3,57 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image</title>
+    <title>image</title>
 </head>
 <body>
+    
     <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="myfile" accept=".pdf , .png , .jpg"><br>
-        <input type="submit" value="Upload" name="btnsub">
+
+    <input type="file" name="myfile" accept=".pdf , .png "><br><br>
+    <input type="submit" value="Upload" name="btnsub">
+
     </form>
 </body>
 </html>
 
 <?php
-if(isset($_POST['btnsub']))
-{   
-    /* Get Original File Name */
-   $filename = $_FILES['myfile']['name'];
 
-    /* Get Temporary File Name */
+if(isset($_POST['btnsub']))
+{
+    /* Orignal File */
+    $filename = $_FILES['myfile']['name'];
+
+    /* Temp File */
     $templocation = $_FILES['myfile']['tmp_name'];
 
-    /*Check File Extension (.pdf , .png)  */
-    echo $fileextenion = strtolower(pathinfo($filename, PATHINFO_EXTENSION ));
+    /* Target Directory */
+    $targetdir = "uploads/".$filename;
 
-    /* Allowed File Extensions */
-    $allowed_extension = array("pdf" , "png");
+    /* Finding File Extension */
+    $fileextention = strtolower(pathinfo($filename , PATHINFO_EXTENSION));
+    $allowerd_extension = array("pdf" , "png");
 
-    /* Check if the file extension is .pdf or .png */
+    /* Finding File Size */
+    $filesize = $_FILES['myfile']['size'];
 
-    if(in_array($fileextenion,$allowed_extension))
+    /* Restricting File Size for 2 MB */
+    $maxsize = 2 * 1024 * 1024;
+
+    /* Checking File Size is greate than 2MB */
+    if($filesize > $maxsize)
     {
-        /* Folder where the file will be saved */
-    $targetdir = "uploads/";
-
-    /* Full path where the file will be saved */
-    $targetpath = $targetdir . $filename;
-
-    /* Move the file from temporary Location To Uploads foder */
-    if(move_uploaded_file($templocation , $targetpath))
-    {
-        echo "Successfully Saved";
+        echo "File is Greater Than 2MB";
     }
+    /* Checking FIle Exntensions */
+    elseif(!in_array($fileextention , $allowerd_extension))
+    {  
+        echo "File Extension Should be .PNG Or .PDF ";
+    }
+
+    /* Uploading File */
     else
     {
-        echo "failed";
+        if(move_uploaded_file($templocation , $targetdir))
+        {
+            echo "Uploaded";
+        }
+        else
+        {
+            echo " Not Uploaded";
+        }
     }
-    }
-
-    else
-    {
-        echo "Incorrect Extension";
-    }
-
-    
 }
+
 ?>
